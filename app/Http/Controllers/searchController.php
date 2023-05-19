1a9b7eab3dc0c4da4712174661508b23b7afc365
kauawb02 <kauabatista254@gmail.com>
2023-05-19 16:27:41 -0300

    Fix: value null

--- app/Http/Controllers/searchController.php
+++ app/Http/Controllers/searchController.php
@@ -11,9 +11,14 @@ class searchController extends Controller
   public function search(Request $request)
   {
     $item = $request->input('item');
-    
-    $item = Accounts_itens::where('item',$item)->groupBy('item')->select('item', DB::raw('SUM(qtd) as total'))->first();
 
-    return response()->json(['mensagem' => 'Requisição AJAX bem-sucedida','item'=>$item->item,'quantity'=>$item->total]);
+    $itemDados = Accounts_itens::where('item', $item)->groupBy('item')->select('item', DB::raw('SUM(qtd) as total'))->first();
+
+    if (empty($itemDados)) {
+      return response()->json(['mensagem' => 'Requisição AJAX bem-sucedida', 'item' => $item, 'quantity' => 0]);
+
+    }
+
+    return response()->json(['mensagem' => 'Requisição AJAX bem-sucedida', 'item' => $itemDados->item, 'quantity' => $itemDados->total]);
   }
 }
